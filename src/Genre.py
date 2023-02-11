4662da0dc86391753b524b77ecb4c1517e7b9941
Utkarsh Jain <67118602+QuantumStatic@users.noreply.github.com>
2023-02-11 18:24:45 +0800

    Added Genre Added methods to add instruments & Genre to the user

---
+++ src/Genre.py
@@ -0,0 +1,46 @@
+from abc import ABC
+
+class Genre(ABC):
+    """Class defining base for genre"""
+    _obj = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._obj is None:
+            cls._obj = super().__new__(cls, *args, **kwargs)
+        return cls._obj
+
+    def __init__(self, name:str):
+        self._name:str = name
+
+    @property
+    def name(self) -> str:
+        """Get the name of the genre"""
+        return self._name
+
+    def __str__(self):
+        return f"Name: {self._name}"
+
+    @classmethod
+    def get_all_genres(cls):
+        """Get all genres"""
+        return tuple(x() for x in Genre.__subclasses__())
+
+class Rock(Genre):
+    """Class defining rock genre"""
+    def __init__(self):
+        super().__init__("Rock")
+
+class Pop(Genre):
+    """Class defining pop genre"""
+    def __init__(self):
+        super().__init__("Pop")
+
+class Jazz(Genre):
+    """Class defining jazz genre"""
+    def __init__(self):
+        super().__init__("Jazz")
+
+class Classical(Genre):
+    """Class defining classical genre"""
+    def __init__(self):
+        super().__init__("Classical")
--- src/User.py
+++ src/User.py
@@ -1,9 +1,20 @@
 from abc import ABC
+from typing import Iterator
+
+from Genre import Genre
+from Instrument import Instrument
+
 
 class User(ABC):
     def __init__(self, name, email):
         self._name:str = name
         self._email:str = email
+        self._genre_list: list[Genre] = []
+        self._instrument_list: list[Instrument] = []
+
+
+    def __str__(self):
+        return f"Name: {self._name}, Email: {self._email}"
 
     @property
     def name(self) -> str:
@@ -14,8 +25,21 @@ class User(ABC):
     def email(self):
         """Get the email of the user"""
         return self._email
+
+    def add_genre(self, genre):
+        """Add a genre to the user"""
+        self._genre_list.append(genre)
+
+    def add_genres(self, genres:Iterator[Genre]):
+        """Add a genre to the user"""
+        for genre in genres:
+            self.add_genre(genre)
     
+    def add_instrument(self, instrument:Instrument):
+        """Add an instrument to the user"""
+        self._instrument_list.append(instrument)
 
-
-    def __str__(self):
-        return f"Name: {self._name}, Email: {self._email}"
+    def add_instruments(self, instruments:Iterator[Instrument]):
+        """Add an instrument to the user"""
+        for instrument in instruments:
+            self.add_instrument(instrument)
